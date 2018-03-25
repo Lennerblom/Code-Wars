e07459cb9bd6289dc76bd37986c96d2d067ee65b
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-03-25 15:53:47 -0700

    inserted dashes

---
+++ insert-dash/solution.js
@@ -0,0 +1,13 @@
+function insertDash(num) {
+  let numString = '';
+  let numArr = num.toString();
+  for(let i = 0; i < numArr.length; i++){
+    if(numArr[i] % 2 === 1 && numArr[i+1] % 2 === 1){
+      numString += numArr[i] + '-';
+    }else{
+      numString += numArr[i];
+    }
+  }
+  return(numString);
+}
+insertDash(45376879);
