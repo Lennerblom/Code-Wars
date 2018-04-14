649c59ebec3fca448ba27b680e125809751fe1a6
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-04-13 17:26:56 -0700

    new branch

--- Code10/solution.js
+++ Code10/solution.js
@@ -3,13 +3,14 @@ function double(array) {
   const dblArr = array.map(elem => elem * 2);
   return dblArr;
 }
-double([3,4,5]);
+double([3,4,5]); //returns [6,8,10]
 
 //Kata 2 (filter)
 function filter_list(l) {
   let noString = l.filter((num) => { return typeof num === 'number';});
   return noString;
 }
+filter_list([3,4,5,'two', 0]); //returns [2,3,4,0]
 
 // Kata 3 
 function find_average(array) {
